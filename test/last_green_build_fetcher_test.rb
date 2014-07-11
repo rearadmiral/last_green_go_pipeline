b43bf031ce8bd31876baa1a4a44e75a2907aee1b
Bill DePhillips <bill.dephillips@gmail.com>
2014-07-11 11:00:49 -0700

    added spec running rake task

--- test/last_green_build_fetcher_test.rb
+++ test/last_green_build_fetcher_test.rb
@@ -1,35 +1,7 @@
-require 'test/unit'
-require 'mocha/setup'
 require_relative '../lib/last_green_build_fetcher'
 require 'ostruct'
 
 class LastGreenBuilderFetcherTest < Minitest::Test
-
-  def setup
-    @cache_filename = File.expand_path(File.join(File.dirname(__FILE__), '..', '.go_watchdog_cache'))
-  end
-
-  def teardown
-    FileUtils.rm_f(@cache_filename)
-  end
-
-  def test_fetching_finds_most_recent_passing_stage
-    mock_pipeline1 = OpenStruct.new
-      passing_units = OpenStruct.new(:name => 'unit', :result => 'Passed', :completed_at => Time.parse('2013-02-10 11:40:00'))
-      failing_acceptance = OpenStruct.new(:name => 'acceptance', :result => 'Failed', :completed_at => Time.parse('2013-02-10 11:45:00'))
-    mock_pipeline1.stages = [passing_units, failing_acceptance]
-
-    mock_pipeline2 = OpenStruct.new
-      passing_units = OpenStruct.new(:name => 'unit', :result => 'Passed', :completed_at => Time.parse('2013-02-11 14:10:00'))
-      passing_acceptance = OpenStruct.new(:name => 'acceptance', :result => 'Passed', :completed_at => Time.parse('2013-02-11 14:19:00'))
-    mock_pipeline2.stages = [passing_units, passing_acceptance]
-
-    with_go_api_client_returning({:pipelines => [mock_pipeline1, mock_pipeline2].reverse, :latest_atom_entry_id => 'ignore'}) do
-      fetcher = LastGreenBuildFetcher.new({:stage_name => 'acceptance'})
-      last_green_build_time = fetcher.fetch
-      assert_equal passing_acceptance.completed_at, last_green_build_time
-    end
-  end
 
   def test_fetch_with_no_green_builds_returns_nil
     failing_pipeline = OpenStruct.new
@@ -80,27 +52,5 @@ class LastGreenBuilderFetcherTest < Minitest::Test
     end
   end
 
-  def self.mock_return_value
-    @@mock_return_value
-  end
-
-  def self.mock_param_value=(param)
-    @@mock_param_value = param
-  end
-
-  private
-
-  def with_go_api_client_returning(return_value)
-    @@mock_return_value = return_value
-    GoApiClient.module_eval do
-      def self.runs(options)
-        LastGreenBuilderFetcherTest.mock_param_value = options
-        LastGreenBuilderFetcherTest.mock_return_value
-      end
-    end
-    yield
-  ensure
-    @@mock_return_value = nil
-  end
 
 end
